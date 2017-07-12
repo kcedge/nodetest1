6ef98b3920d2487917de1df25485397859cce4aa
Casey Edge <edge.casey@gmail.com>
2017-07-12 17:42:06 -0600

    production flag controller update

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -10,7 +10,7 @@ $(".facebookLink").children().first().attr("target", "_blank");
 $(".twitterLink").children().first().attr("target", "_blank");
 
 //angular.module("myApp", ["$scope","$http", "ngCookies"]);
-var runningProduction = false;
+var runningProduction = true;
 function isAuthenticated($http, routeToSignUp, callback) {
     var req = {
 	method: 'GET',
